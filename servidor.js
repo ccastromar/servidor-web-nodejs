c86e09c229038362124cca33b4cacc4a0cd03867
carlos <carlos@xarleecm.com>
2017-03-02 09:55:26 +0100

    Creacion de los ficheros

---
+++ servidor.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var enrutador = require('httpdispatcher');
+var fs = require('fs');
+
+//maneja una petición
+function handleRequest(request, response){
+        console.log("Peticion:");
+        console.log(request.url);
+        enrutador.dispatch(request, response);
+
+}
+//creamos el servidor y lo ponemos a escuchar en el puerto 8888
+var server = http.createServer(handleRequest);
+server.listen(8888, function(){
+    console.log("Arrancado y escuchando en el puerto 8888");
+});
+
+//creamos la función que trata un GET de la raiz en el enrutador 
+//para que cargue el fichero index.html y lo devuelva en la respuesta
+enrutador.onGet("/", function(req, res) {
+
+   var retHtml = function(content) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(content, 'utf-8');
+   }
+
+   var retError = function(code, content) {
+     res.writeHead(code, {'Content-Type': 'text/plain'});
+     res.end(content);
+   }
+   fs.readFile('./index.html', function(error, content) {
+      if (error) {
+        retError(500);
+      } else {
+        retHtml(content);
+      }
+   });
+});
